5d21b74c5f441c922571547de91fb7e6eb136076
leselim-12 <charbelabouassly0@gmail.com>
2026-02-06 22:22:20 +0200

    Initial Commit

---
+++ backend/server.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import joblib
+from pydantic import BaseModel
+import pandas as pd
+import numpy as np
+
+app = FastAPI() #creating fastapi object
+ridge_pipeline = joblib.load("../model/ridge_pipeline.joblib")  # loading the saved pipeline
+
+class FWIRequest(BaseModel): #Creating the base model of the request for validation
+    Temperature: float
+    RH: float
+    Ws: float
+    Rain: float
+    FFMC: float
+    DMC: float
+    DC: float
+    ISI: float
+     
+origins = [
+    "http://localhost:3000" 
+]
+
+app.add_middleware( #allowing communications with the react frontend
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/health') #check api status
+def check_health():
+    return {'status' : 'ok'}
+
+@app.post('/predict')
+def get_prediction(data : FWIRequest):
+    df = pd.DataFrame([data.dict()])
+    prediction = np.expm1(ridge_pipeline.predict(df))
+    return {'prediction': round(prediction[0],2)}
